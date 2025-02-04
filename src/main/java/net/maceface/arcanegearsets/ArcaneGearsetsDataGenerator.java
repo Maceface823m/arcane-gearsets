45f6379475dcbfc95e2a3d041c6322b9b800eb55
Maceface823m <keefemaceo2@gmail.com>
2025-02-04 14:00:02 -0800

    commit 2

--- src/main/java/net/maceface/arcanegearsets/ArcaneGearsetsDataGenerator.java
+++ src/main/java/net/maceface/arcanegearsets/ArcaneGearsetsDataGenerator.java
@@ -2,7 +2,7 @@ package net.maceface.arcanegearsets;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
+// comment
 public class ArcaneGearsetsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
