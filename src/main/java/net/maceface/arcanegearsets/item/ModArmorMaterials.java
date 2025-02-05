c6fb0755c18921222c3456b5b98e6e24cc4bfdef
Maceface823m <keefemaceo2@gmail.com>
2025-02-04 18:47:47 -0800

    added prismarinne upgrade template. no texture yet.

---
+++ src/main/java/net/maceface/arcanegearsets/item/ModArmorMaterials.java
@@ -0,0 +1,10 @@
+package net.maceface.arcanegearsets.item;
+
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.registry.entry.RegistryEntry;
+
+public class ModArmorMaterials {
+
+    public static RegistryEntry<ArmorMaterial> registerArmorMaterial
+
+}
