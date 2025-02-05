a2eb5848026b065b08917f0d1d7af11443114865
Maceface823m <keefemaceo2@gmail.com>
2025-02-05 15:00:39 -0800

    a lot

--- src/main/java/net/maceface/arcanegearsets/item/ModItems.java
+++ src/main/java/net/maceface/arcanegearsets/item/ModItems.java
@@ -1,17 +1,30 @@
 package net.maceface.arcanegearsets.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.maceface.arcanegearsets.ArcaneGearsets;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
+
 
 public class ModItems {
 
     public static final Item PRISMARINE_UPGRADE_TEMPLATE = registerItem("prismarine_upgrade_template", new Item(new Item.Settings()));
+            //new SmithingTemplateItem(
+                    //Text.translatable("upgrade.yourmodid.prismarine_upgrade"), // Title
+                    //Text.translatable("upgrade.yourmodid.prismarine_upgrade_applies_to"), // Applies to
+                    //Text.translatable("upgrade.yourmodid.prismarine_upgrade_ingredients"), // Ingredients
+                    //Text.translatable("upgrade.yourmodid.prismarine_upgrade_base_slot"), // Base slot
+                    //Text.translatable("upgrade.yourmodid.prismarine_upgrade_additions_slot"), // Additions slot
+                    //List.of(Items.DIAMOND_HELMET, Items.DIAMOND_CHESTPLATE, Items.DIAMOND_LEGGINGS, Items.DIAMOND_BOOTS), // Base items
+                    //List.of(new Identifier("minecraft", "heart_of_the_sea")) // Addition items
+
+
 
     public static final Item PRISMARINE_HELMET = registerItem("prismarine_helmet",
             new ArmorItem(ModArmorMaterials.PRISMARINE_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings()
@@ -34,6 +47,10 @@ public class ModItems {
         return Registry.register(Registries.ITEM, Identifier.of(ArcaneGearsets.MOD_ID, name), item);
     }
 
+    public static String getUpgradeDescription(String key) {
+        return "upgrade.arcane" + key;
+    }
+
     public static void registerModItems(){
         ArcaneGearsets.LOGGER.info("Restering Mod Items For " + ArcaneGearsets.MOD_ID);
 
@@ -41,6 +58,5 @@ public class ModItems {
             fabricItemGroupEntries.add(PRISMARINE_UPGRADE_TEMPLATE);
         });
     }
-
 
 }
