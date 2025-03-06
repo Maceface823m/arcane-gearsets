c3b12e30bf1b6790e117e7217938a3d1cbd77528
Maceface823m <keefemaceo2@gmail.com>
2025-03-05 23:08:19 -0800

    finished potions

--- src/main/java/net/maceface/arcanegearsets/ArcaneGearsets.java
+++ src/main/java/net/maceface/arcanegearsets/ArcaneGearsets.java
@@ -4,11 +4,14 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.maceface.arcanegearsets.effect.ModEffects;
+import net.maceface.arcanegearsets.effect.WitherResistanceEffect;
 import net.maceface.arcanegearsets.item.ModItemGroups;
 import net.maceface.arcanegearsets.item.ModItems;
 import net.maceface.arcanegearsets.potion.ModPotions;
 import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +26,17 @@ public class ArcaneGearsets implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModEffects.registerEffects();
+		ModPotions.registerPotions();
 
 		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-			builder.registerPotionRecipe(Potions.AWKWARD, Items.WITHER_ROSE, ModPotions.WITHER_RESISTANCE_POTION);
+			builder.registerPotionRecipe(
+					Potions.AWKWARD,
+					Items.WITHER_ROSE,
+					ModPotions.WITHER_RESISTANCE_POTION
+			);
 		});
+
+
+
 	}
 }
--- src/main/java/net/maceface/arcanegearsets/potion/ModPotions.java
+++ src/main/java/net/maceface/arcanegearsets/potion/ModPotions.java
@@ -13,8 +13,8 @@ public class ModPotions {
 
     public static final RegistryEntry<Potion> WITHER_RESISTANCE_POTION = registerPotion("wither_resistance_potion",
             new Potion(new StatusEffectInstance(ModEffects.WITHER_RESISTANCE, 3600, 0)));
-    //public static final RegistryEntry<Potion> WITHER_RESISTANCE_POTION = registerPotion("wither_resistance_potion",
-            //new Potion(new StatusEffectInstance(ModEffects.WITHER_RESISTANCE, 9600, 0)));
+    public static final RegistryEntry<Potion> LONG_WITHER_RESISTANCE_POTION = registerPotion("long_wither_resistance_potion",
+            new Potion(new StatusEffectInstance(ModEffects.WITHER_RESISTANCE, 9600, 0)));
 
     private static RegistryEntry<Potion> registerPotion(String name, Potion potion) {
         return Registry.registerReference(Registries.POTION, Identifier.of(ArcaneGearsets.MOD_ID, name), potion);
